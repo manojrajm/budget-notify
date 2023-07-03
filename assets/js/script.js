d80f71c97c9c89f85df13774b96897e1f4df0133
manojrajm <gauthamtamziha007@gmail.com>
2023-07-03 22:32:49 +0530

    adding extrra data

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,7 +24,8 @@ function addEntry() {
     var entry = {
       amount: amount,
       item: item,
-      type: type
+      type: type,
+      date: new Date().toLocaleString() // Add current date and time to the entry
     };
 
     entries.push(entry);
@@ -38,12 +39,25 @@ function addEntry() {
     updateGraphData();
     renderEntries();
     renderCurrentAmount();
-    saveToStorage();
     amountInput.value = '';
     itemInput.value = '';
   } else {
     alert('Please enter a valid amount and item.');
   }
+  saveData();
+}
+function saveData() {
+  localStorage.setItem('entries', JSON.stringify(entries));
+  localStorage.setItem('currentAmount', currentAmount);
+  localStorage.setItem('graphLabels', JSON.stringify(graphLabels));
+  localStorage.setItem('graphData', JSON.stringify(graphData));
+}
+
+function resetData() {
+  // ... Your existing resetData() function code ...
+
+  // Clear data from local storage
+  clearData();
 }
 
 function saveToStorage() {
@@ -69,26 +83,95 @@ function updateGraphData() {
   graphLabels.push(label);
   graphData.push(totalIncome - totalExpenses);
 }
+function load() {
+  var storedEntries = localStorage.getItem('entries');
+  var storedCurrentAmount = localStorage.getItem('currentAmount');
+  var storedGraphLabels = localStorage.getItem('graphLabels');
+  var storedGraphData = localStorage.getItem('graphData');
+
+  if (storedEntries && storedCurrentAmount && storedGraphLabels && storedGraphData) {
+    entries = JSON.parse(storedEntries);
+    currentAmount = Number(storedCurrentAmount);
+    graphLabels = JSON.parse(storedGraphLabels);
+    graphData = JSON.parse(storedGraphData);
+  }
+
+  renderEntries();
+  renderCurrentAmount();
+  renderGraph();
+}
+
 
 function renderEntries() {
   var entriesList = document.getElementById('entries-list');
   entriesList.innerHTML = '';
 
-  entries.forEach(function (entry) {
+  entries.forEach(function (entry, index) {
     var entryItem = document.createElement('div');
-    entryItem.textContent = entry.item + ' (' + entry.type + '): ' + entry.amount;
+    entryItem.textContent = entry.item + ' (' + entry.type + '): ' + entry.amount + '--- ' + entry.date; ;
     entryItem.className = 'entry-item';
+
+    // Create a delete button for each entry
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.style.width = "120px";
+    deleteButton.style.padding =" 0 20px";
+    deleteButton.style.float ="right";
+    deleteButton.onclick = function () {
+      deleteEntry(index);
+    };
+    entryItem.appendChild(deleteButton);
+
     entriesList.appendChild(entryItem);
   });
+}
+
+function deleteEntry(index) {
+  if (index >= 0 && index < entries.length) {
+    var deletedEntry = entries.splice(index, 1)[0];
+    if (deletedEntry.type === 'income') {
+      currentAmount -= deletedEntry.amount;
+    } else if (deletedEntry.type === 'expenses') {
+      currentAmount += deletedEntry.amount;
+    }
+    updateGraphData();
+    renderEntries();
+    renderCurrentAmount();
+    saveData();
+  }
+}
+function resetData() {
+  entries = [];
+  currentAmount = 0;
+  graphLabels = [];
+  graphData = [];
+  clearData(); // Clear data from local storage
+  renderEntries();
+  renderCurrentAmount();
+}
+
+function clearData() {
+  localStorage.removeItem('entries');
+  localStorage.removeItem('currentAmount');
+  localStorage.removeItem('graphLabels');
+  localStorage.removeItem('graphData');
 }
 
 function renderCurrentAmount() {
   var currentAmountElement = document.getElementById('current-amount');
   currentAmountElement.textContent = 'Current Amount: ' + currentAmount;
+  renderEntries();
+  saveToStorage();
+
 }
 
 function renderGraph() {
   var graphCanvas = document.getElementById('graph');
+  
+  // Destroy the existing chart if it exists
+  if (Chart.instances.length > 0) {
+    Chart.instances[0].destroy();
+  }
 
   new Chart(graphCanvas, {
     type: 'line',
@@ -113,6 +196,7 @@ function renderGraph() {
     }
   });
 }
+
 
 function confirmShare() {
   if (navigator.share) {
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Budget-Notify 
+
+## Technologies
+  - HTMl
+  - Css
+  - Javascript
+  - Bootstrap
+  - JS Libraries(chart.js)
+
+  ## Purpose
+  It is a simple website for notify the amount how much u expenses or income .
+  It is easy to add a purpose for spending and earning the amount .
+  Very easy to calculate the wallet
+
+  ## Preview
+
+
+
